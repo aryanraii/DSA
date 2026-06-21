9eeb90cebbab292d4d66b7e57e4501cf21ad59b1
aryanraii <aryanraii0410@gmail.com>
2026-06-21 17:40:25 +0530

    DutchNationalFlagAlgo

---
+++ src/Array/DutchNationalFlagAlgo.java
@@ -0,0 +1,28 @@
+package Array;
+
+public class DutchNationalFlagAlgo {
+    /// Dutch National Flag Algorithm--> Tc=O(n) but in single pass
+    public void sortColors(int[] nums) {
+        int low=0;
+        int mid=0;
+        int high=nums.length-1;
+        while(mid<=high){
+            if(nums[mid]==0){
+                int temp=nums[low];
+                nums[low]=nums[mid];
+                nums[mid]=temp;
+                low++;
+                mid++;
+            }
+            else if(nums[mid]==1){
+                mid++;
+            }
+            else if(nums[mid]==2){
+                int temp=nums[high];
+                nums[high]=nums[mid];
+                nums[mid]=temp;
+                high--;
+            }
+        }
+    }
+}
