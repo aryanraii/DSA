fa12b910122a37ebd31bc2450d667c8a7248927d
aryanraii <aryanraii0410@gmail.com>
2026-07-01 01:37:08 +0530

    Lc 485

---
+++ src/Array/MaxConsecutiveOnes_Lc485.java
@@ -0,0 +1,19 @@
+package Array;
+
+public class MaxConsecutiveOnes_Lc485 {
+    ///  Time complexity-->O(n);
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int n=nums.length;
+        int max=0;
+        int count=0;
+        for(int i=0; i<n; i++){
+            if(nums[i]==1){
+                count++;
+                max=Math.max(max,count);
+            }else{
+                count=0;
+            }
+        }
+        return max;
+    }
+}
