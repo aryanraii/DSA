02180b0260e0bccaa0b65b5d1d95c7f046346852
aryanraii <aryanraii0410@gmail.com>
2026-06-30 23:16:09 +0530

    Lc 189

---
+++ src/Array/RotateArray_Lc189.java
@@ -0,0 +1,35 @@
+package Array;
+
+public class RotateArray_Lc189 {
+    ///  Time Complexity-->O(N);
+    public void rotate(int[] nums, int k) {
+        int n=nums.length;
+        int start=0;
+        int end=n-1;
+        while(start<end){
+            int temp=nums[start];
+            nums[start]=nums[end];
+            nums[end]=temp;
+            start++;
+            end--;
+        }
+        start=0;
+        end=k-1;
+        while(start<end){
+            int temp=nums[start];
+            nums[start]=nums[end];
+            nums[end]=temp;
+            start++;
+            end--;
+        }
+        start=k;
+        end=n-1;
+        while(start<end){
+            int temp=nums[start];
+            nums[start]=nums[end];
+            nums[end]=temp;
+            start++;
+            end--;
+        }
+    }
+}
