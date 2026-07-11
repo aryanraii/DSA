b0392227e054152b354ba529ad23acb046803e9c
aryanraii <aryanraii0410@gmail.com>
2026-07-11 11:31:51 +0530

    LL

---
+++ src/LinkedList/MiddleoftheLinkedList_Lc876.java
@@ -0,0 +1,13 @@
+package LinkedList;
+
+public class MiddleoftheLinkedList_Lc876 {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null&&fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
+    }
+}
