761f87cf792b3cc8626baefabe4418fff339c1e4
aryanraii <aryanraii0410@gmail.com>
2026-07-11 18:54:05 +0530

    Lc 19

--- src/LinkedList/RemoveNthNodeFromEndofList_Lc19.java
+++ src/LinkedList/RemoveNthNodeFromEndofList_Lc19.java
@@ -2,25 +2,27 @@ package LinkedList;
 
 public class RemoveNthNodeFromEndofList_Lc19 {
     //Time complexity-->O(2n) space complexity-->O(1)
-    public ListNode removeNthFromEnd(ListNode head, int n) {
-        int size=0;
-        ListNode temp=head;
-        while(temp!=null){//O(n)
-            size++;
-            temp=temp.next;
-        }
-        if(size==n){
-            head=head.next;
-            return head;
-        }
-        int idx=1;
-        temp=head;
-        while(idx<size-n){
-            idx++;
-            temp=temp.next;
-        }
-        temp.next=temp.next.next;
-
-        return head;
-    }
+//    public ListNode removeNthFromEnd(ListNode head, int n) {
+//        int size=0;
+//        ListNode temp=head;
+//        while(temp!=null){//O(n)
+//            size++;
+//            temp=temp.next;
+//        }
+//        if(size==n){
+//            head=head.next;
+//            return head;
+//        }
+//        temp=head;
+//        int reach=size-n;
+//        while(temp!=null){
+//            reach--;
+//            if(reach==0){
+//                break;
+//            }
+//            temp=temp.next;
+//        }
+//        temp.next=temp.next.next;
+//        return head;
+//    }
 }
